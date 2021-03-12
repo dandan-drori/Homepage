4a971fbb1f37f83463b2939bd3b2978268653e9d
Dandan Drori <55210781+dandan-drori@users.noreply.github.com>
2021-03-12 02:44:26 +0200

    initial commit

---
+++ bookmarks.js
@@ -0,0 +1,47 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "Inbox", url: "https://inbox.google.com" },
+      { name: "GitHub", url: "https://github.com" },
+      { name: "Drive", url: "https://drive.google.com" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "Netflix", url: "https://netflix.com" },
+      { name: "Crunchyroll", url: "https://crunchyroll.com" },
+    ],
+  },
+  {
+    title: "Useful",
+    links: [
+      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
+      { name: "Commit Emojis", url: "https://gitmoji.dev" },
+      {
+        name: "Emojis Unicode",
+        url: "https://unicode.org/emoji/charts/full-emoji-list.html",
+      },
+      {
+        name: "Math in MD",
+        url: "https://rpruim.github.io/s341/S19/from-class/MathinRmd.html",
+      },
+      { name: "MD Math", url: "luvxuan.top/posts/Markdown-math/" },
+    ],
+  },
+  {
+    title: "School",
+    links: [
+      { name: "Moodle", url: "https://lemida.biu.ac.il" },
+      { name: "Inbar", url: "https://inbar.biu.ac.il/live/Main.aspx" },
+      { name: "Piazza", url: "https://piazza.com/class/kkzbow4lvwx2uc" },
+      {
+        name: "Submit",
+        url: "https://submit.cs.biu.ac.il/cgi-bin/welcome.cgi",
+      },
+    ],
+  },
+];
