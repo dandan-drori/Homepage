2a01b75c44f8050c32127a19c54e478aa157f278
Dandan Drori <55210781+dandan-drori@users.noreply.github.com>
2021-03-26 18:23:37 +0300

    add icons to links and fixed links numbering

--- bookmarks.js
+++ bookmarks.js
@@ -3,39 +3,107 @@ const bookmarks = [
   {
     title: "Daily",
     links: [
-      { name: "Inbox", url: "https://inbox.google.com" },
-      { name: "GitHub", url: "https://github.com" },
-      { name: "Drive", url: "https://drive.google.com" },
+      {
+        name: "GitHub",
+        url: "https://github.com",
+        imgSize: "30",
+        imgSrc:
+          "file:/home/dandan/Installed Programs/Homepage/icons/github.png",
+      },
+      {
+        name: "Youtube",
+        url: "https://youtube.com",
+        imgSize: "30",
+        imgSrc:
+          "file:/home/dandan/Installed Programs/Homepage/icons/youtube.png",
+      },
+      {
+        name: "Netflix",
+        url: "https://netflix.com",
+        imgSize: "30",
+        imgSrc:
+          "file:/home/dandan/Installed Programs/Homepage/icons/netflix.png",
+      },
     ],
   },
   {
-    title: "Media",
+    title: "Linux",
     links: [
-      { name: "Youtube", url: "https://youtube.com" },
-      { name: "Netflix", url: "https://netflix.com" },
-      { name: "Crunchyroll", url: "https://crunchyroll.com" },
+      {
+        name: "Arch",
+        url: "https://archlinux.org",
+        imgSize: "30",
+        imgSrc: "file:/home/dandan/Installed Programs/Homepage/icons/arch.png",
+      },
+      {
+        name: "Stackoverflow",
+        url: "https://stackoverflow.com",
+        imgSize: "30",
+        imgSrc:
+          "file:/home/dandan/Installed Programs/Homepage/icons/stackoverflow.png",
+      },
+      {
+        name: "Cronjob",
+        url: "https://cron.help/#1_0_*_*_*",
+        imgSize: "30",
+        imgSrc: "file:/home/dandan/Installed Programs/Homepage/icons/cron.png",
+      },
     ],
   },
   {
     title: "Useful",
     links: [
-      { name: "Commit Emojis", url: "https://gitmoji.dev" },
+      {
+        name: "Commit Emojis",
+        url: "https://gitmoji.dev",
+        imgSize: "30",
+        imgSrc: "file:/home/dandan/Installed Programs/Homepage/icons/emoji.png",
+      },
       {
         name: "Math in MD",
         url: "https://rpruim.github.io/s341/S19/from-class/MathinRmd.html",
+        imgSize: "30",
+        imgSrc:
+          "file:/home/dandan/Installed Programs/Homepage/icons/markdown.png",
       },
-      { name: "MD Math", url: "luvxuan.top/posts/Markdown-math/" },
+      {
+        name: "MD Math",
+        url: "luvxuan.top/posts/Markdown-math/",
+        imgSize: "30",
+        imgSrc:
+          "file:/home/dandan/Installed Programs/Homepage/icons/markdown.png",
+      },
     ],
   },
   {
     title: "School",
     links: [
-      { name: "Moodle", url: "https://lemida.biu.ac.il" },
-      { name: "Inbar", url: "https://inbar.biu.ac.il/live/Main.aspx" },
-      { name: "Piazza", url: "https://piazza.com/class/kkzbow4lvwx2uc" },
+      {
+        name: "Moodle",
+        url: "https://lemida.biu.ac.il",
+        imgSize: "30",
+        imgSrc:
+          "file:/home/dandan/Installed Programs/Homepage/icons/moodle.png",
+      },
+      {
+        name: "Inbar",
+        url: "https://inbar.biu.ac.il/live/Main.aspx",
+        imgSize: "30",
+        imgSrc: "file:/home/dandan/Installed Programs/Homepage/icons/test.png",
+      },
+      {
+        name: "Piazza",
+        url: "https://piazza.com/class/kkzbow4lvwx2uc",
+        imgSize: "30",
+        imgSrc:
+          "file:/home/dandan/Installed Programs/Homepage/icons/piazza.png",
+      },
       {
         name: "Submit",
         url: "https://submit.cs.biu.ac.il/cgi-bin/welcome.cgi",
+        imgSize: "30",
+        imgSrc:
+          "file:/home/dandan/Installed Programs/Homepage/icons/submit.png",
       },
     ],
   },
